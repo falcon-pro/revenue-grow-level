ed118f16f3a964d7896cbb5d9101ed7198457348
devspeed2k <devspeed2k@gmail.com>
2025-05-20 02:57:44 +0500

    Fix Bug

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -335,18 +335,7 @@ async function closeDeleteModal() {
         </svg>
         <h3 class="mt-2 text-lg font-medium text-gray-900">No partners yet</h3>
         <p class="mt-1 text-sm text-gray-500">Get started by adding your first partner.</p>
-        <div class="mt-6">
-          <button 
-            type="button"
-            on:click={openAddModal}
-            class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-            </svg>
-            Add Partner
-          </button>
-        </div>
+      
       </div>
     {/if}
   </div>
